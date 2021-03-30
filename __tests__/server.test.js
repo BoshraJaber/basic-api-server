163180aa1ec7db0c7febf9cf41915a4473f4c779
Boshra Jaber <boshrajaber94@gmail.com>
2021-03-30 23:08:16 +0300

    testing for clothes

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,6 +5,7 @@ const { report } = require('../src/ routes/clothes');
 const server = require('../src/ server');
 const request = superTest(server.server);
 let id;
+let idClothes;
 
 describe('Server', () => {
   it('handle invalid routes', async () => {
@@ -17,7 +18,7 @@ describe('Server', () => {
     const response = await request.get('/error');
     expect(response.status).toEqual(500);
   });
-  // checking for routes status code and returned value
+  // checking for routes status code and returned value for Food
   //Create a record
   it('Create a record', async () => {
     const response = await (await request.post('/api/v1/food/').send({
@@ -29,7 +30,7 @@ describe('Server', () => {
     expect(response.body.data.price).toEqual('5');
     id = response.body.id
   });
-  // Update a record
+  // Update a record 
   it('Update a record', async () => {
     const response = await request.put(`/api/v1/food/${id}`).send({
       type :'fast food',
@@ -57,6 +58,51 @@ describe('Server', () => {
   // Delete a record
   it('Delete a record', async () => {
     const response = await request.delete(`/api/v1/food/${id}`);
+    expect(response.status).toEqual(200);
+    // console.log(response.body);
+    expect(response.body).toEqual([]);
+  });
+  //=======================================
+  // checking for routes status code and returned value for Food
+  //Create a record
+  it('Create a record', async () => {
+    const response = await (await request.post('/api/v1/clothes/').send({
+        type : 'healthy',
+        price : '5',
+    }))
+    expect(response.status).toEqual(201);
+    expect(response.body.data.type).toEqual('healthy');
+    expect(response.body.data.price).toEqual('5');
+    idClothes = response.body.id
+  });
+  // Update a record 
+  it('Update a record', async () => {
+    const response = await request.put(`/api/v1/clothes/${idClothes}`).send({
+      type :'fast food',
+      price : '6',
+    });
+    expect(response.status).toEqual(200);
+    expect(response.body.data.type).toEqual('fast food');
+    expect(response.body.data.price).toEqual('6');
+  });
+  // Read a record
+  it('Read a record', async () => {
+    const response = await request.get(`/api/v1/clothes/${idClothes}`);
+    expect(response.status).toEqual(200);
+    expect(response.body.data.type).toEqual('fast food');
+    expect(response.body.data.price).toEqual('6');
+  });
+  // Read all Records
+  it('Read all record', async () => {
+    const response = await request.get('/api/v1/clothes/');
+    expect(response.status).toEqual(200);
+    // console.log(response.body[0]);
+    expect(response.body[0].data.type).toEqual('fast food');
+    expect(response.body[0].data.price).toEqual('6');
+  });
+  // Delete a record
+  it('Delete a record', async () => {
+    const response = await request.delete(`/api/v1/clothes/${idClothes}`);
     expect(response.status).toEqual(200);
     // console.log(response.body);
     expect(response.body).toEqual([]);
